6bf13372cd036bd73f5a0c243932b701f038c337
jfw2855 <jwaddle1130@gmail.com>
2022-08-31 10:54:50 -0500

    adds manufacturer model

---
+++ api/models/manufacturer.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class Manufacturer(models.Model):
+  # define fields
+  name = models.CharField(max_length=100)
+
+  def __str__(self):
+    
+    return f"{self.name}"
+
+  def as_dict(self):
+    return {
+        'id': self.id,
+        'name': self.name
+    }
