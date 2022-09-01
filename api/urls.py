b844772c42b43e4f560c0e9472e03884ee287f8a
jfw2855 <jwaddle1130@gmail.com>
2022-08-31 21:29:40 -0500

    removes trailing slashes

--- api/urls.py
+++ api/urls.py
@@ -5,12 +5,12 @@ from .views.experiment_views import Experiments, ExpDetails
 
 urlpatterns = [
   	# Restful routing
-    path('mangos/', Mangos.as_view(), name='mangos'),
-    path('mangos/<int:pk>/', MangoDetail.as_view(), name='mango_detail'),
+    path('mangos', Mangos.as_view(), name='mangos'),
+    path('mangos/<int:pk>', MangoDetail.as_view(), name='mango_detail'),
     path('sign-up/', SignUp.as_view(), name='sign-up'),
     path('sign-in/', SignIn.as_view(), name='sign-in'),
     path('sign-out/', SignOut.as_view(), name='sign-out'),
     path('change-pw/', ChangePassword.as_view(), name='change-pw'),
-    path('experiments/',Experiments.as_view(), name='experiments'),
-    path('experiments/<int:pk>/', ExpDetails.as_view(), name='exp_detail'),
+    path('experiments',Experiments.as_view(), name='experiments'),
+    path('experiments/<int:pk>', ExpDetails.as_view(), name='exp_detail'),
 ]
