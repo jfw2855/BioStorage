8672de05339f0095516583c48f385ac2066efe36
jfw2855 <jwaddle1130@gmail.com>
2022-09-06 15:15:13 -0500

    adds url path to storage type details class

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,7 @@ from .views.mango_views import Mangos, MangoDetail
 from .views.user_views import SignUp, SignIn, SignOut, ChangePassword
 from .views.experiment_views import Experiments, ExpDetails
 from .views.storage_views import Storages
-from .views.storage_type_views import StorageTypes
+from .views.storage_type_views import StorageTypes, StorageTypeDetails
 
 urlpatterns = [
   	# Restful routing
@@ -17,4 +17,5 @@ urlpatterns = [
     path('experiments/<int:pk>', ExpDetails.as_view(), name='exp_detail'),
     path('storages', Storages.as_view(), name='storages'),
     path('storage_types',StorageTypes.as_view(), name='storage_types'),
+    path('storage_types/<int:pk>',StorageTypeDetails.as_view(), name='storage_type_details'),
 ]
