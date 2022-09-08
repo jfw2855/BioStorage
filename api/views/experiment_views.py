6bfeb0ce8e265b8c3c2a7ae6032f77d5fba0476e
jfw2855 <jwaddle1130@gmail.com>
2022-09-08 12:46:18 -0500

    adds patch route to ExpDetails class

--- api/views/experiment_views.py
+++ api/views/experiment_views.py
@@ -36,7 +36,6 @@ class ExpDetails (generics.RetrieveUpdateDestroyAPIView):
     permission_classes = (IsAuthenticated,)
     
     def get(self, request, pk):
-        print('this is req',request)
         """Show request: get data then return serialized data"""
         exp = get_object_or_404(Experiment, pk=pk)
         data = ExperimentSerializer(exp).data
@@ -45,12 +44,26 @@ class ExpDetails (generics.RetrieveUpdateDestroyAPIView):
         return Response({'experiment':data})
     
     def delete(self,request, pk):
-        print('this is req',request)
         """Delete Request: deletes requester's exp data from db then return 204"""
         exp = get_object_or_404(Experiment, pk=pk)
-
         if request.user != exp.owner:
             raise PermissionDenied("Unauthorized, you do not own this experiment") 
         exp.delete()
-
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def partial_update(self, request, pk):
+        """Update Request: updates the experiment then returns 204"""
+        exp = get_object_or_404(Experiment, pk=pk)
+        # Checks if user is exp owner
+        if request.user != exp.owner:
+            raise PermissionDenied('Unauthorized, you do not own this experiment')
+        # Ensure the owner field is set to the current user's ID
+        request.data['experiment']['owner'] = request.user.id
+        # Validate updates with serializer
+        data = ExperimentSerializer(exp, data=request.data['experiment'], partial=True)
+        if data.is_valid():
+            # Save & send a 204 no content
+            data.save()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        # If the data is not valid, return a response with the errors
+        return Response(data.errors, status=status.HTTP_400_BAD_REQUEST)
