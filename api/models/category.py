1a17018b3a791f5b4cd3a77885cf0e34cf0eef2d
jfw2855 <jwaddle1130@gmail.com>
2022-09-11 14:19:01 -0500

    creates category model

---
+++ api/models/category.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Category(models.Model):
+
+  name = models.CharField(max_length=100, unique=True)
+
+  def __str__(self):
+    
+    return f"{self.name}"
+
+  def as_dict(self):
+    return {
+        'id': self.id,
+        'name': self.name
+    }
