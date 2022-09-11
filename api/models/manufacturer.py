e4fe7a0ef4bce23c782d718a9cc30e3a138ff4b9
jfw2855 <jwaddle1130@gmail.com>
2022-09-10 22:41:40 -0500

    makes name field unique

--- api/models/manufacturer.py
+++ api/models/manufacturer.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Manufacturer(models.Model):
   # define fields
-  name = models.CharField(max_length=100)
+  name = models.CharField(max_length=100, unique=True)
 
   def __str__(self):
     
