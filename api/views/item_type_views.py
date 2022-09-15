01eba6ce9e168982d9dfe57524d4c68603d9ba88
jfw2855 <jwaddle1130@gmail.com>
2022-09-15 13:05:19 -0500

    adds Item Type Details class with get, delete, and update routes

--- api/views/item_type_views.py
+++ api/views/item_type_views.py
@@ -28,3 +28,30 @@ class ItemTypes(generics.ListCreateAPIView):
         # If the data is not valid, return a response with the errors
         return Response(type.errors, status=status.HTTP_400_BAD_REQUEST)
 
+"""Item Type Details Class: GET, DELETE, UPDATE specific item types (by PK)"""
+class ItemTypeDetails(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes=(IsAuthenticated,)
+    def get(self, request, pk):
+        """Show request: Gets specific item type from db and returns serialized data"""
+        type = get_object_or_404(ItemType, pk=pk)
+        data = ItemTypeSerializer(type).data
+        return Response({ 'item_type': data })
+    
+    def delete(self,request,pk):
+        """Delete request: deletes item type from db then returns 204"""
+        type = get_object_or_404(ItemType,pk=pk)
+        type.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def partial_update(self, request, pk):
+        """Update Request: updates item type in db then returns 204"""
+        type = get_object_or_404(ItemType,pk=pk)
+
+        # Validate updates with serializer
+        data = ItemTypeSerializer(type, data=request.data['item_type'], partial=True)
+        if data.is_valid():
+            # Save & send a 204 no content
+            data.save()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        # If the data is not valid, return a response with the errors
+        return Response(data.errors, status=status.HTTP_400_BAD_REQUEST)
